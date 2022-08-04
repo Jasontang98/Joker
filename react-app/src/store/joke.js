60b492ed0a75a6237f21cbaa54176f769d028c12
Jasontang98 <jasontang@DESKTOP-K3NMI40.localdomain>
2022-08-04 09:19:39 -0700

    jokes

--- react-app/src/store/joke.js
+++ react-app/src/store/joke.js
@@ -32,7 +32,7 @@ const deleteJoke = (joke) => ({
 // Get All Jokes
 export const getAllJokes = () => async (dispatch) => {
     console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    const response = await fetch(`/api/jokes`);
+    const response = await fetch('/api/jokes/');
 
     if (response.ok) {
         const data = await response.json();
